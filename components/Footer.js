5ea8c90fba4b7d526d4383057bffbdbf9eb58e23
Jotish Sharma <jotishsharma03@gmail.com>
2021-08-22 15:21:54 +0530

    clone-airbnb index

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+function Footer() {
+    return (
+       <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-800">
+           <div className="space-y-4 text-xs text-gray-80 ">
+            <h5 className="font-bold">ABOUT</h5>
+           <p>How Airbnb works</p>
+           <p>Newsroom</p>
+           <p>Investors</p>
+           <p>Airbnb Plus</p>
+           <p>Airbnb Luxe</p>
+           </div>
+
+           <div className="space-y-4 text-xs text-gray-80 ">
+            <h5 className="font-bold">COMMUNITY</h5>
+           <p>Accessibility</p>
+           <p>This is just a clone</p>
+           <p>Pretty Awesome clone </p>
+           <p>Payments accepted</p>
+           <p>Just NextJS</p>
+           </div>
+
+           <div className="space-y-4 text-xs text-gray-80 ">
+            <h5 className="font-bold">HOST</h5>
+           <p>Here we are</p>
+           <p>Our works</p>
+           <p>Support us</p>
+           <p>Email Us</p>
+           <p>Social Airbnb</p>
+           </div>
+
+           <div className="space-y-4 text-xs text-gray-80 ">
+            <h5 className="font-bold">SUPPORT</h5>
+           <p>Airbnb Supports</p>
+           <p>Chatroom</p>
+           <p>Rules Next</p>
+           <p>Cascading Styles</p>
+           <p>Scripters</p>
+           </div>
+
+        
+       </div>
+    )
+}
+
+export default Footer
